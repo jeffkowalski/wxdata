77059e92a4d09c0d7b338335a97d647cfe02dd74
Jeff Kowalski <jeff.kowalski@gmail.com>
2022-02-16 22:44:59 -0800

    use botbase

--- Gemfile
+++ Gemfile
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
+gem 'botbase', git: 'https://github.com/jeffkowalski/botbase'
+
 source 'https://rubygems.org'
-
-gem 'influxdb'
-gem 'thor'
-
-# for robe:
-gem 'method_source'
-gem 'pry'
-gem 'pry-doc'
--- ws1001.rb
+++ ws1001.rb
@@ -1,28 +1,9 @@
 #!/usr/bin/env ruby
 # frozen_string_literal:true
 
-require 'thor'
-require 'fileutils'
-require 'logger'
-require 'socket'
-require 'influxdb'
-
-LOGFILE = File.join(Dir.home, '.log', 'ws1001.log')
-
-module Kernel
-  def with_rescue(exceptions, logger, retries: 5)
-    try = 0
-    begin
-      yield try
-    rescue *exceptions => e
-      try += 1
-      raise if try > retries
-
-      logger.info "caught error #{e.class}, retrying (#{try}/#{retries})..."
-      retry
-    end
-  end
-end
+require 'rubygems'
+require 'bundler/setup'
+Bundler.require(:default)
 
 UDPPORT = 6000 # udp port  # broadcast message port
 BCMSG   = ['PC2000', 'SEARCH', '', 0, 0].pack 'Z8 Z8 Z16 I I'
@@ -60,32 +41,7 @@ FIELDS  = [
 ].freeze
 # rubocop: enable Layout/ExtraSpacing, Layout/SpaceInsideParens, Style/NumericLiterals
 
-class WS1001 < Thor
-  no_commands do
-    def redirect_output
-      unless LOGFILE == 'STDOUT'
-        logfile = File.expand_path(LOGFILE)
-        FileUtils.mkdir_p(File.dirname(logfile), mode: 0o755)
-        FileUtils.touch logfile
-        File.chmod 0o644, logfile
-        $stdout.reopen logfile, 'a'
-      end
-      $stderr.reopen $stdout
-      $stdout.sync = $stderr.sync = true
-    end
-
-    def setup_logger
-      redirect_output if options[:log]
-
-      @logger = Logger.new $stdout
-      @logger.level = options[:verbose] ? Logger::DEBUG : Logger::INFO
-      @logger.info 'starting'
-    end
-  end
-
-  class_option :log,     type: :boolean, default: true, desc: "log output to #{LOGFILE}"
-  class_option :verbose, type: :boolean, aliases: '-v', desc: 'increase verbosity'
-
+class WS1001 < RecorderBotBase
   no_commands do
     def query_panel(verb)
       @logger.info 'opening udp socket'
@@ -138,40 +94,35 @@ class WS1001 < Thor
     end
   end
 
-  desc 'record-status', 'record the current usage data to database'
-  option :station, type: :string, default: '<broadcast>', desc: 'ip addr of weather station'
-  def record_status
-    setup_logger
+  class_option :station, type: :string, default: '<broadcast>', desc: 'ip addr of weather station' # FIXME: should be option
+  no_commands do
+    def main
+      rcvmsg = query_panel(SNDMSG)
+      timestamp = Time.now.to_i
+      # File.open('/tmp/nowrecord', 'w') { |file| file.write(rcvmsg) }
 
-    rcvmsg = query_panel(SNDMSG)
-    timestamp = Time.now.to_i
-    File.open('/tmp/nowrecord', 'w') { |file| file.write(rcvmsg) }
+      # Unpack NOWRECORD message received from console
+      packing = (FIELDS.collect { |field| field[:pack] }).join ''
+      @logger.info "unpacking #{packing}"
+      msgcontent = rcvmsg.unpack packing
 
-    # Unpack NOWRECORD message received from console
-    packing = (FIELDS.collect { |field| field[:pack] }).join ''
-    @logger.info "unpacking #{packing}"
-    msgcontent = rcvmsg.unpack packing
+      (0..FIELDS.length - 1).each do |index|
+        @logger.info FIELDS[index][:name].ljust(21) +
+                     msgcontent[index].class.to_s.ljust(10) +
+                     msgcontent[index].to_s
+      end
 
-    (0..FIELDS.length - 1).each do |index|
-      @logger.info FIELDS[index][:name].ljust(21) +
-                   msgcontent[index].class.to_s.ljust(10) +
-                   msgcontent[index].to_s
-    end
-
-    influxdb = InfluxDB::Client.new 'wxdata'
-    (0..FIELDS.length - 1).each do |index|
-      value = msgcontent[index]
-      if !FIELDS[index].key?(:validator) || FIELDS[index][:validator].call(value)
-        data = { values: { value: value }, timestamp: timestamp }
-        influxdb.write_point FIELDS[index][:name], data unless value.nil?
-      else
-        @logger.warn "#{FIELDS[index][:name]} #{value} is out of valid range"
+      influxdb = InfluxDB::Client.new 'wxdata'
+      (0..FIELDS.length - 1).each do |index|
+        value = msgcontent[index]
+        if !FIELDS[index].key?(:validator) || FIELDS[index][:validator].call(value)
+          data = { values: { value: value }, timestamp: timestamp }
+          influxdb.write_point FIELDS[index][:name], data unless value.nil?
+        else
+          @logger.warn "#{FIELDS[index][:name]} #{value} is out of valid range"
+        end
       end
     end
-    @logger.info 'done'
-  rescue StandardError => e
-    @logger.error "caught exception #{e}"
-    @logger.error e.backtrace.join("\n")
   end
 end
 
